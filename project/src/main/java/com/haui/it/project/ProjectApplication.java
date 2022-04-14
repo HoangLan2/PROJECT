b341e7c77cec7e30efbd4c6a3448563e2e5c4ae6
tuans <tuans.fit@gmail.com>
2022-04-14 23:06:17 +0800

    Update ProjectApplication.java

--- project/src/main/java/com/haui/it/project/ProjectApplication.java
+++ project/src/main/java/com/haui/it/project/ProjectApplication.java
@@ -2,10 +2,8 @@ package com.haui.it.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication /* (exclude = SecurityConfiguration.class) */
-@ComponentScan(basePackages= { "com.haui.it.project.controller.admin" })
 public class ProjectApplication {
 
 	public static void main(String[] args) {
